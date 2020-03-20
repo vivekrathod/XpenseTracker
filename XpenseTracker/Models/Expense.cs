33fb10c8194df64def132511c76906539b30ba08
Vivek Rathod <vrathod@trustwave.com>
2020-03-20 00:14:54 -0400

    Initial models (Expense and ExpenseCategory)

---
+++ XpenseTracker/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XpenseTracker.Models
+{
+    public class Expense
+    {
+        public UInt64 Id { get; set; }
+        public DateTime Date { get; set; }
+        public ExpenseCategory Category { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ XpenseTracker/Models/ExpenseCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XpenseTracker.Models
+{
+    public class ExpenseCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool UserDefined { get; set; }
+    }
+}
